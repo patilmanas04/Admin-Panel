f6e3edb4e6f0004b7b3f6cf2bca908a99c640d3b
Manas Patil <patilmanas1304@gmail.com>
2024-05-18 00:57:00 +0530

    Add files and modified sign in page

---
+++ src/app/main/sign-in/tabs/JwtSignInTab.jsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import JwtSignInForm from '../../../auth/services/jwt/components/JwtSignInForm';
+
+function jwtSignInTab() {
+	return <JwtSignInForm />;
+}
+
+export default jwtSignInTab;
